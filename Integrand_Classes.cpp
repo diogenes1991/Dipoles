5adb3c5a9561649be88d4b573d94a64434f02223
Diogenes <diogenes1991@gmail.com>
2019-12-03 15:56:31 -0500

    Multivariate Targets updatedIntegrand_Classes.h

--- Integrand_Classes.cpp
+++ Integrand_Classes.cpp
@@ -1,18 +1,26 @@
 #include <cmath>
 #include <iostream>
-#include "Integrand_Classes.h"
+#include "Integrand_Classes_2.h"
 
 
 using namespace std;
 
 Plus_Distribution Curly_G_ia;
 
+double My_Divergent(double* x){return x[0]*2+x[1]-x[2];}
+double My_Finite(double* x){return x[0]*x[0];}
 
 
 main(int argc, char* argv[]){        
 
+    Curly_G_ia.set_D(&My_Divergent);
+    Curly_G_ia.set_R(&My_Finite);
+    
+    double vars[4]={0.5,2,7,3};
+    
 
-    cout << "Curly_G_ia(0.5) = " << Curly_G_ia.eval_D(0.5) << " , " << Curly_G_ia.eval_R(0.5) << endl;
+    ~Curly_G_ia;
+    cout << "Curly_G_ia(0.5) = " << Curly_G_ia.eval_D(vars) << " , " << Curly_G_ia.eval_R(vars) << endl;
 
 
 }                                      
