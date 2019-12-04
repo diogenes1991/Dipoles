d90eacbe09d929e21e137533162cde08e73ec8dc
Diogenes <diogenes1991@gmail.com>
2019-12-03 22:55:44 -0500

    Safety Checks for Recursive PSP are implemented to avoid segmentation faults

--- Recursive_PSPG.cpp
+++ Recursive_PSPG.cpp
@@ -3,16 +3,28 @@
 #include <cmath>
 #include "Vector_Real.h"
 #include "Phase_Spaces_Real.h"
+#include <time.h>
 
 
 main(int argc, char* argv[]){
     
+
+
+if((argc-1)<4||(argc-1)<(2+atoi(argv[1]))){
+    cout << "Error: Not enough arguments"<<endl
+    << "Please provide the arguments in the following order:"<<endl
+    << "1) The number of particles N" <<endl
+    << "2) The CM Energy" <<endl
+    << "3) The N masses of the particles "<<endl;
+    abort();
+}
+
+
 const int NPar = atoi(argv[1]);
 const double sqrts = atof(argv[2]);
 
 Vector PList[NPar];
 double MList[NPar];
-
 
 
 for (int i=0;i<NPar;i++){MList[i]=atof(argv[i+3]);}
@@ -22,6 +34,8 @@ for (int i=0;i<NPar;i++){MList[i]=atof(argv[i+3]);}
 Vector P(sqrts,0,0,0);
 double ran[3*NPar-4];
 
+
+srand(time(NULL));
 for (int i=0;i<(3*NPar-4);i++){ran[i]=aleatorio(0,1);}
 
 
