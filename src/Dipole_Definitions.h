90a8dc46e70defb9725997811a46deafd9ab91a8
Diogenes Figueroa <diogenes1991@gmail.com>
2020-11-22 23:02:19 -0500

    Name changes to fermion and boson dipoles

--- src/Dipole_Definitions.h
+++ src/Dipole_Definitions.h
@@ -6,6 +6,8 @@
 #include <gsl/gsl_sf_dilog.h>
 #define PolyLog2 gsl_sf_dilog
 
+#define TESTMODE 0
+
 // 
 //  The log_4pi definition is needed if the NLO Cross-Sections
 //  are defined not to have (4pi)^epsilon/Gamma(1-epsilon) upfront
@@ -14,19 +16,6 @@
 
 #define log_4pi 2.5310242469692907929778915942694118477
 #define pi2 9.869604401089358618834490999876151135
-
-template<class T>
-FourMatrixT<T> TensorProduct(FourVectorT<T> a, FourVectorT<T> b){
-    FourMatrixT<T> Mat;
-    T flata[4] = {a.p0,a.p1,a.p2,a.p3};
-    T flatb[4] = {b.p0,b.p1,b.p2,b.p3};
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            Mat.M[i][j] = flata[i]*flatb[j];
-        }
-    }
-    return Mat;
-}
 
 // 
 //   II Functions and Maps 
@@ -77,15 +66,34 @@ FourVectorT<T> Boost_II(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k, 
 }
 
 template <class T>
-void Build_II_Momenta(int NEXT_BORN, std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>> P_TIL, int EMIT, int SPEC, int RADI){
-    P_TIL.at(EMIT) = pa_II(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
-    for(int II=2;II<NEXT_BORN;II++){
-        P_TIL.at(II) = Boost_II(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI),P_DAT.at(II));
+void Build_II_Momenta(std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>>* P_TIL, int EMIT, int SPEC, int RADI){
+    P_TIL->clear();
+    int count = 0;
+    for(FourVectorT<T> v: P_DAT){
+        if(count != RADI) P_TIL->push_back(v);
+        count++;
     }
+    P_TIL->at(EMIT) = pa_II(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+    for(unsigned int II=2;II<P_TIL->size();II++){
+        P_TIL->at(II) = Boost_II(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI),P_DAT.at(II));
+    }
+    #if TESTMODE
+        count = 0;
+        std::cout<<"IF Momenta Validation:"<<std::endl;
+        for (FourVectorT<T> v : *P_TIL){
+            std::cout<<"p"<<count+1<<" = ";
+            v.print();
+            int i = count;
+            if (i>=RADI) i++;
+            std::cout<<"m"<<count+1<<"^2 = "<<v*v<<"  ("<<P_DAT.at(i)*P_DAT.at(i)<<")"<<std::endl;
+            count++;
+        }
+        std::cout<<std::endl;
+    #endif
 }
 
 template <class T>
-T g_ab_fermion(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k, T ma, T mb){
+T g_ab_ffb(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k, T ma, T mb){
     T xab = x_ab(pa,pb,k);
     T sab = ma*ma+mb*mb+2*(pa*pb);
     if (xab<xab_cut(sab,ma,mb)) return 0;
@@ -97,7 +105,7 @@ T g_ab_fermion(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k, T ma, T m
 }
 
 template <class T>
-FourMatrixT<T> g_ab_boson(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k){
+FourMatrixT<T> g_ab_bbb(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k, T ma, T mb){
     FourMatrixT<T> Matrix;
     T xab = x_ab(pa,pb,k);
     FourVectorT<T> kproj = k - ((pa*k)/(pa*pb))*pb;
@@ -111,7 +119,7 @@ FourMatrixT<T> g_ab_boson(FourVectorT<T> pa, FourVectorT<T> pb, FourVectorT<T> k
 }
 
 template <class T>
-T CurlyG_ab_fermion(T sab, T ma, T mb, T x, T Ix, T I1){
+T CurlyG_ab_ffb(T sab, T ma, T mb, T mu, T x, T Ix, T I1){
     
     T Rval = 0;
     T Fx,F1;
@@ -120,7 +128,6 @@ T CurlyG_ab_fermion(T sab, T ma, T mb, T x, T Ix, T I1){
     T LOG_DR = /*log_4pi + */log(mu*mu*sab/(sab_bar*sab_bar));
 
     if (ma==0.0){
-
         Fx = (1.0-x)*(1.0-x)-(1.0+x*x)*LOG_DR*Ix/x;
         F1 = -2*LOG_DR*I1;
         Rval += (1.0/(1.0-x))*(Fx-F1);
@@ -128,7 +135,6 @@ T CurlyG_ab_fermion(T sab, T ma, T mb, T x, T Ix, T I1){
         Fx = 2.0*(1.0+x*x)*LOG_DR*Ix/x;
         F1 = 4.0*LOG_DR*I1;
         Rval += (log(1.0-x)/(1.0-x))*(Fx-F1);
-
     }
 
     else{
@@ -140,12 +146,10 @@ T CurlyG_ab_fermion(T sab, T ma, T mb, T x, T Ix, T I1){
         F1 = 2.0*(1.0+sab_bar/sqrt(lambda_ab)*LOG)*I1;
         Rval += (1.0/(1.0-x))*(Fx-F1);
     }
-
-
-};
+}
 
 template <class T>
-void G_ab_fermion(T sab, T ma, T mb, T mu, T* RVAL){
+void G_ab_ffb(T sab, T ma, T mb, T mu, T* RVAL){
 
     // TODO: This template only works for T=double due to the gsl_sf_dilog call
  
@@ -154,14 +158,12 @@ void G_ab_fermion(T sab, T ma, T mb, T mu, T* RVAL){
     T LOG_DR = /*log_4pi + */log(mu*mu*sab/(sab_bar*sab_bar)) - 2*log(1.0-x0);
 
     if (ma==0.0){
-
         RVAL[0] = 1;
         RVAL[1] = - 3.0/2.0 - LOG_DR - x0*(1.0+x0)/2.0;
         RVAL[2] = LOG_DR*LOG_DR - pi2 / 4;
     }
 
     else{
-
         T BETA = sab_bar + 2*ma*ma - sqrt(lambda(sab,ma*ma,mb*mb));
           BETA = BETA / (sab_bar + 2*ma*ma + sqrt(lambda(sab,ma*ma,mb*mb)));
         T  LOG = log(BETA);
@@ -170,10 +172,36 @@ void G_ab_fermion(T sab, T ma, T mb, T mu, T* RVAL){
         RVAL[1] = - 1 - sab_bar/lambda(sab,ma*ma,mb*mb)*LOG;
         RVAL[2] = (2*PolyLog2(1-BETA) + (1.0/2.0)*LOG*LOG + ((sab_bar+2*ma*ma)/(sab_bar))*LOG);
         RVAL[2] = LOG_DR*RVAL[1]-(sab_bar/lambda(sab,ma*ma,mb*mb))*RVAL[2];
+    }
+}
 
+template <class T>
+void G_ab_bbb(T sab, T ma, T mb, T mu, T* RVAL){
+
+    // TODO: This template only works for T=double due to the gsl_sf_dilog call
+ 
+    T sab_bar = sab - ma*ma - mb*mb;
+    T x0 = xab_cut(sab,ma,mb);
+    T LOG_DR = /*log_4pi + */log(mu*mu*sab/(sab_bar*sab_bar)) - 2*log(1.0-x0);
+
+    if (ma==0.0){
+        RVAL[0] = 1;
+        RVAL[1] = - 3.0/2.0 - LOG_DR - x0*(1.0+x0)/2.0;
+        RVAL[2] = LOG_DR*LOG_DR - pi2 / 4;
     }
 
+    else{
+        T BETA = sab_bar + 2*ma*ma - sqrt(lambda(sab,ma*ma,mb*mb));
+          BETA = BETA / (sab_bar + 2*ma*ma + sqrt(lambda(sab,ma*ma,mb*mb)));
+        T  LOG = log(BETA);
+        
+        RVAL[0] = 0;
+        RVAL[1] = - 1 - sab_bar/lambda(sab,ma*ma,mb*mb)*LOG;
+        RVAL[2] = (2*PolyLog2(1-BETA) + (1.0/2.0)*LOG*LOG + ((sab_bar+2*ma*ma)/(sab_bar))*LOG);
+        RVAL[2] = LOG_DR*RVAL[1]-(sab_bar/lambda(sab,ma*ma,mb*mb))*RVAL[2];
+    }
 }
+
 
 //
 //  IF & FI Functions and Maps 
@@ -194,8 +222,7 @@ T z_ia(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k){
 template <class T>
 T R_ia(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k, T x){
     FourVectorT<T> P_ai = pi - pa + k;
-    T P_ai_bar = (P_ai*P_ai) - (pa*pa) - (pi*pi);
-    
+    T P_ai_bar = (P_ai*P_ai) - (pa*pa) - (pi*pi);   
     T out = (P_ai_bar + 2*(pa*pa)*x)*(P_ai_bar + 2*(pa*pa)*x);
     out = out - 4*(pa*pa)*(P_ai*P_ai)*x*x;
     out = out / (lambda((P_ai*P_ai),(pa*pa),(pi*pi)));
@@ -204,7 +231,7 @@ T R_ia(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k, T x){
 }
 
 template <class T>
-T g_ai_fermion(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k){
+T g_ai_ffb(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k, T ma, T mi){
   
     T xai = x_ia(pa,pi,k);
     T zai = z_ia(pa,pi,k);
@@ -221,7 +248,7 @@ T g_ai_fermion(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k){
 }
 
 template <class T>
-T g_ia_fermion(FourVectorT<T> pi, FourVectorT<T> pa, FourVectorT<T> k){
+T g_ia_ffb(FourVectorT<T> pi, FourVectorT<T> pa, FourVectorT<T> k, T mi, T ma){
     
     T xia = x_ia(pa,pi,k);
     T zia = z_ia(pa,pi,k);
@@ -233,17 +260,17 @@ T g_ia_fermion(FourVectorT<T> pi, FourVectorT<T> pa, FourVectorT<T> k){
     return out;   
 }
 
-// NOTE: Currently g_ia(ai)_boson are a placeholders and are overloaded to g_ia(ai)_fermion temporarily //
+// NOTE: Currently g_ia(ai)_bbb are a placeholders and are overloaded to g_ia(ai)_ffb temporarily //
 template <class T>
-T g_ai_boson(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k){
+T g_ai_bbb(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k, T ma, T mi){
     
-    return g_ai_fermion(pa,pi,k);    
+    return g_ai_ffb(pa,pi,k,ma,mi);    
 }
 
 template <class T>
-T g_ia_boson(FourVectorT<T> pi, FourVectorT<T> pa, FourVectorT<T> k){
+T g_ia_bbb(FourVectorT<T> pi, FourVectorT<T> pa, FourVectorT<T> k, T mi, T ma){
     
-    return g_ia_fermion(pi,pa,k);
+    return g_ia_ffb(pi,pa,k,mi,ma);
 }
 
 template <class T>
@@ -258,21 +285,122 @@ FourVectorT<T> pi_IFFI(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k){
 
 template <class T>
 FourVectorT<T> pa_IFFI(FourVectorT<T> pa, FourVectorT<T> pi, FourVectorT<T> k){
-  
   return pi_IFFI(pa,pi,k)- pi - k + pa ;  
 }
 
 template <class T>
-void Build_IF_Momenta(int NEXT_BORN, std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>> P_TIL, int EMIT, int SPEC, int RADI){
-    P_TIL.at(EMIT) = pa_IFFI(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
-    P_TIL.at(SPEC) = pi_IFFI(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+void Build_IF_Momenta(std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>>* P_TIL, int EMIT, int SPEC, int RADI){
+    P_TIL->clear();
+    int count = 0;
+    for(FourVectorT<T> v: P_DAT){
+        if(count != RADI) P_TIL->push_back(v);
+        count++;
+    }
+    int e = EMIT;
+    int s = SPEC;
+    if (EMIT>RADI) e-=1;
+    if (SPEC>RADI) s-=1;
+    P_TIL->at(e) = pa_IFFI(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+    P_TIL->at(s) = pi_IFFI(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+    #if TESTMODE
+        count = 0;
+        std::cout<<"IF Momenta Validation:"<<std::endl;
+        for (FourVectorT<T> v : *P_TIL){
+            std::cout<<"p"<<count+1<<" = ";
+            v.print();
+            int i = count;
+            if (i>=RADI) i++;
+            std::cout<<"m"<<count+1<<"^2 = "<<v*v<<"  ("<<P_DAT.at(i)*P_DAT.at(i)<<")"<<std::endl;
+            count++;
+        }
+        std::cout<<std::endl;
+    #endif
+
 }
 
 template <class T>
-void Build_FI_Momenta(int NEXT_BORN, std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>> P_TIL, int EMIT, int SPEC, int RADI){
-    P_TIL.at(EMIT) = pi_IFFI(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
-    P_TIL.at(SPEC) = pa_IFFI(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+void Build_FI_Momenta(std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>>* P_TIL, int EMIT, int SPEC, int RADI){
+    P_TIL->clear();
+    int count = 0;
+    for(FourVectorT<T> v: P_DAT){
+        if(count != RADI) P_TIL->push_back(v);
+        count++;
+    }
+    int e = EMIT;
+    int s = SPEC;
+    if (EMIT>RADI) e-=1;
+    if (SPEC>RADI) s-=1;
+    P_TIL->at(e) = pi_IFFI(P_DAT.at(SPEC),P_DAT.at(EMIT),P_DAT.at(RADI));
+    P_TIL->at(s) = pa_IFFI(P_DAT.at(SPEC),P_DAT.at(EMIT),P_DAT.at(RADI));
+    #if TESTMODE
+        std::cout<<"FI Momenta Validation:"<<std::endl;
+        count = 0;
+        for (FourVectorT<T> v : *P_TIL){
+            std::cout<<"p"<<count+1<<" = ";
+            v.print();
+            int i = count;
+            if (i>=RADI) i++;
+            std::cout<<"m"<<count+1<<"^2 = "<<v*v<<"   ("<<P_DAT.at(i)*P_DAT.at(i)<<")"<<std::endl;
+            count++;
+        }
+        std::cout<<std::endl;
+    #endif
 }
+
+template <class T>
+T Curly_G_ia_ffb(T  P_ia, T mi, T ma, T mu, T x, T Ix, T I1){
+    
+    T P_ia_bar = P_ia - mi*mi - ma*ma;
+    
+    T aux1 = 1 - (2*mi*mi*x)/(P_ia_bar*(1-x)) + z(P_ia,ma,mi,x,-1)/2;
+    aux1 = aux1 * (-z(P_ia,ma,mi,x,-1));
+    aux1 = aux1 - 2*Log( 2 - x - z(P_ia,ma,mi,x,-1));
+    
+    T aux2 = 1 - (2*mi*mi*x)/(P_ia_bar*(1-x)) + z(P_ia,ma,mi,x,1)/2;
+    aux2 = aux2 * (-z(P_ia,ma,mi,x,1));
+    aux2 = aux2 - 2*Log( 2 - x - z(P_ia,ma,mi,x,1));
+    
+    aux1 = aux1 - aux2;
+    aux1 = aux1 * (-P_ia_bar)/(Sqrt(Lambda(P_ia,mi*mi,ma*ma))*R_ia(P_ia,ma,mi,x)*(1-x));
+    
+    return aux1;
+       
+    
+}
+
+
+template <class T>
+void G_ia_ffb(T Pia, T mi, T ma, T mu, T* RVAL){
+
+    RVAL[0] = 0;
+    RVAL[1] = 0;
+    RVAL[2] = 0;
+}
+
+template <class T>
+void G_ai_ffb(T Pia, T ma, T mi, T mu, T* RVAL){
+
+    RVAL[0] = 0;
+    RVAL[1] = 0;
+    RVAL[2] = 0;
+}
+
+template <class T>
+void G_ia_bbb(T Pia, T mi, T ma, T mu, T* RVAL){
+
+    RVAL[0] = 0;
+    RVAL[1] = 0;
+    RVAL[2] = 0;
+}
+
+template <class T>
+void G_ai_bbb(T Pia, T ma, T mi, T mu, T* RVAL){
+
+    RVAL[0] = 0;
+    RVAL[1] = 0;
+    RVAL[2] = 0;
+}
+
 
 // 
 //  FF Functions and Maps
@@ -305,7 +433,7 @@ T R_ij(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k,T y){
 }
 
 template <class T>
-T g_ij_fermion(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k){
+T g_ij_ffb(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k, T mi, T mj){
     T zij = z_ij(pi,pj,k);
     T yij = y_ij(pi,pj,k);
     T aux = zij*(1.0-yij);
@@ -318,8 +446,8 @@ T g_ij_fermion(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k){
 }
 
 template <class T>
-T g_ij_boson(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k){
-    return g_ij_fermion(pi,pj,k);  
+T g_ij_bbb(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k){
+    return g_ij_ffb(pi,pj,k);  
 }
 
 template <class T>
@@ -339,14 +467,28 @@ FourVectorT<T> pi_FF(FourVectorT<T> pi, FourVectorT<T> pj, FourVectorT<T> k){
 }
 
 template <class T>
-void Build_FF_Momenta(int NEXT_BORN, std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>> P_TIL, int EMIT, int SPEC, int RADI){
-    P_TIL.at(EMIT) = pi_FF(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
-    P_TIL.at(SPEC) = pj_FF(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
-    for(int II=0;II<NEXT_BORN;II++){
-        if ( II!=EMIT && II!=SPEC ){
-            P_TIL.at(II) = P_DAT.at(II);
-        }
+void Build_FF_Momenta(std::vector<FourVectorT<T>> P_DAT, std::vector<FourVectorT<T>>* P_TIL, int EMIT, int SPEC, int RADI){
+    P_TIL->clear();
+    int count = 0;
+    for(FourVectorT<T> v: P_DAT){
+        if(count != RADI) P_TIL->push_back(v);
+        count++;
     }
+    P_TIL->at(EMIT) = pi_FF(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+    P_TIL->at(SPEC) = pj_FF(P_DAT.at(EMIT),P_DAT.at(SPEC),P_DAT.at(RADI));
+    // for(unsigned int II=0;II<P_TIL->size();II++){
+    //     if ( II!=EMIT && II!=SPEC ){
+    //         P_TIL->at(II) = P_DAT.at(II);
+    //     }
+    // }
+}
+
+template <class T>
+void G_ij_ffb(T sij, T mi, T mj, T mu, T* RVAL){
+
+    RVAL[0] = 0;
+    RVAL[1] = 0;
+    RVAL[2] = 0;
 }
 
 #endif
