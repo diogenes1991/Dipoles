2c54f3bf35d96e7a08c5b8141faa7f4914a4a0fe
Diogenes Figueroa <diogenes1991@gmail.com>
2020-11-22 22:51:38 -0500

    Installation procdures for LHAPDF

--- util/install_utils.sh
+++ util/install_utils.sh
@@ -5,6 +5,7 @@ LOGFILE=install_utils.log
 
 GSL=gsl-2.6
 CUBA=Cuba-4.2
+LHAPDF=LHAPDF-6.3.0
 
 echo "--------------------------------------------" >> $LOGFILE
 echo "------- Installing all dependancies --------" >> $LOGFILE
@@ -32,6 +33,8 @@ echo "Compiling GSL (this may take a while)"
 make -j8 >> $LOGFILE 2>&1
 mv $LOGFILE ../
 cd ..
+
+sed 's/####GSL PATH####/${PWD}/built/${GSL}/g' "../tpl/makefile"
 
 LIBGSL=${GSL}/.libs/libgsl.a
 
@@ -87,3 +90,43 @@ else
     echo "CUBA installation successful"
     mv $CUBA built
 fi
+
+echo "" >> $LOGFILE
+echo "" >> $LOGFILE
+echo "" >> $LOGFILE
+
+echo "--------------------------------------------" >> $LOGFILE
+echo "---------- Now working on LHAPDF -----------" >> $LOGFILE
+echo "--------------------------------------------" >> $LOGFILE
+
+echo "" >> $LOGFILE
+
+echo "Extracting LHAPDF"
+tar xvzf ${LHAPDF}.tar.gz >> $LOGFILE
+mv $LOGFILE $LHAPDF
+cd $LHAPDF
+echo "Configuring LHAPDF"
+./configure --prefix="${PWD}/built" >> $LOGFILE
+echo "Compiling LHAPDF (this may take a while)"
+make -j8 >> $LOGFILE 2>&1
+make install >> $LOGFILE 2>&1
+mv $LOGFILE ../
+cd ..
+
+LIBLHAPDF=${LHAPDF}/built/lib/libLHAPDF.a
+
+if [ ! -f "$LIBLHAPDF" ]
+then
+    echo ""
+    echo "Error: ${LIBLHAPDF} not found."
+    echo "       Please check utilities installation."
+    echo ""
+    echo ""
+    echo "" >> $LOGFILE
+    echo "" >> $LOGFILE
+    exit 1
+else
+    echo "LHAPDF installation successful"
+    mv $LHAPDF built
+fi
+
