2747b9411a4d2b130b66cdab4a45e182a7f4b8c2
Diogenes Figueroa <diogenes1991@gmail.com>
2020-11-27 12:59:27 -0500

    Addition of analysis files to inplement cuts more easily

---
+++ src/Analysis.cpp
@@ -0,0 +1,11 @@
+#include "Analysis.h"
+
+void Analysis::ReweightEvent(FourVector* ExtMom, double ExtMass, int* ExtPID, int BornNext, double* weight){
+    
+    double wgt = 1;
+    
+    
+
+    *weight = wgt;
+
+}
