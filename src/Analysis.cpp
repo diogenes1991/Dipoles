b5e2a8eed84d54ee8097164b00ac347229bfcd9d
Diogenes Figueroa <diogenes1991@gmail.com>
2020-11-28 21:52:09 -0500

    Sample analysis file

--- src/Analysis.cpp
+++ src/Analysis.cpp
@@ -1,11 +1,46 @@
 #include "Analysis.h"
+#include <vector>
 
-void Analysis::ReweightEvent(FourVector* ExtMom, double ExtMass, int* ExtPID, int BornNext, double* weight){
+void Analysis::ReweightEvent(FourVector* ExtMom, double* ExtMass, int* ExtPID, int BornNext, double* weight){
     
+    // This is a sample Analysis file
+
     double wgt = 1;
-    
-    
 
+    ///////////////////////////////////////////////////
+    //
+    // Gets all A and g particles from a channel
+    // it cuts on the energy of them below some amount
+    //
+    ///////////////////////////////////////////////////
+
+    // std::vector<FourVector> radiation_mom;
+    // for(int i=2;i<BornNext;i++){
+    //     if(std::abs(ExtPID[i])==21||std::abs(ExtPID[i])==0)radiation_mom.push_back(ExtMom[i]);
+    // }
+
+    // for(FourVector p : radiation_mom){
+    //     if( p.p0 < 1) wgt = 0;
+    // }
+
+    //////////////////////////////////////////////////
+    //
+    // Gets all final b-quarks from a channel and cuts 
+    // on both its eta and transverse momentum
+    //
+    ///////////////////////////////////////////////////
+
+    // std::vector<FourVector> final_bs_mom;
+    // for(int i=2;i<BornNext;i++){
+    //     if(std::abs(ExtPID[i])==5)final_bs_mom.push_back(ExtMom[i]);
+    // }
+
+    // for(FourVector p : final_bs_mom){
+    //     double eta = std::abs(Kinematics::PseudoRapidity(p));
+    //     double pt = Kinematics::TransverseMomentum(p);
+    //     if( pt < 25 || eta > 2.5 ) wgt = 0;
+    // }
+    
     *weight = wgt;
 
 }
