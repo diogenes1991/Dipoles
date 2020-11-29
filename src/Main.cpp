cb164a0b54cc808c1629cbc5130b13cbb297fb0e
Diogenes Figueroa <diogenes1991@gmail.com>
2020-11-28 21:46:46 -0500

    Update to use new classes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,4 @@
-#include "XSection.h"
+#include "XSection_Integrator.h"
 
 void XSectionStart(std::string pdfname){
 
@@ -41,26 +41,37 @@ void LoadInput(const std::string & filename, std::unordered_map<std::string,std:
 
 int main(int argc, char* argv[]){
 
-    std::unordered_map<std::string,std::string> Settings;
+    if( argc < 2){
+        std::cout<<"Error: No input file specified"<<std::endl;
+        abort();
+    }
+    if( argc >2){
+        std::cout<<"Error: Too many arguments"<<std::endl;
+        abort();
+    }
 
+    std::unordered_map<std::string,std::string> Settings;
     LoadInput(argv[1],Settings);
-    
+            
     std::string PDFSet = Settings.at("LHAPDFSet");
 
     std::string Integrator = Settings.at("Integrator");
     std::string Method = Settings.at("Method");
 
-    XSecCalc::xsec_sel XSC;
+    Montecarlo_Integrator::Specifications MC_SP;
+    MC_SP.MaxEval = stoi(Settings.at("NEvaluations"));
+    MC_SP.NStart = stoi(Settings.at("NStart"));
+    MC_SP.NIncrease = stoi(Settings.at("NIncrease"));
 
-    std::string Integrand = Settings.at("Integrand");
-    std::string Channel = Settings.at("Channel");
-    std::string Coupling = Settings.at("Coupling");
-    
+    XSection_Integrator::XSection_Selector XS;
+    XS.Integrand = Settings.at("Integrand");
+    XS.Channel = Settings.at("Channel");
+    XS.Coupling = Settings.at("Coupling");
 
     XSectionStart(PDFSet);
     X1->SetScales(13000,91.1876,91.1876);
-    XSecCalc C1(Integrator); // Careful one can set up for seg-fault by not initializong an integrator but calling it anyways
-    C1.ComputeXSections(Integrand,Channel,Coupling,Integrator,Method);
+    XSection_Integrator C1(Integrator);
+    C1.ComputeXSection(XS,MC_SP,Method);
     XSectionEnd();
 
     return 0;
