85e76198bccbd3d2ba00d9b077ab4620ed4cb2e0
Diogenes Figueroa <diogenes1991@gmail.com>
2020-11-28 21:54:18 -0500

    Upadate to use new XSection.h selection methods

--- src/XSection_Integrator.h
+++ src/XSection_Integrator.h
@@ -23,6 +23,7 @@ class XSection_Integrator{
         std::string Channel;
         std::string Integrand;
         std::string Coupling;
+        std::string Catalog;
         };
 
         XSection_Integrator(std::string Integrator){
@@ -59,7 +60,7 @@ class XSection_Integrator{
             double rval;
             XSection_Selector xs;
             xs = *(XSection_Selector*)(param);
-            X1->SetXSection(xs.Integrand,xs.Channel,xs.Coupling,x,&rval);
+            X1->SetXSection(xs.Catalog,xs.Integrand,xs.Channel,xs.Coupling,x,&rval);
             return rval;
         }
 
@@ -70,23 +71,44 @@ class XSection_Integrator{
             size_t dim = *ndim;
             double y[dim];
             for(size_t i=0;i<dim;i++)y[i]=x[i];
-            X1->SetXSection(xs.Integrand,xs.Channel,xs.Coupling,y,&rval);
+            X1->SetXSection(xs.Catalog,xs.Integrand,xs.Channel,xs.Coupling,y,&rval);
             f[0] = rval;
             return 0;
         }
 
-        void ComputeXSection(std::string Integrand, std::string Channel, std::string Coupling, std::string Integrator, std::string Method){
+        void ComputeXSection(XSection_Selector XS, Montecarlo_Integrator::Specifications MC, std::string Method){
             Montecarlo_Integrator::Specifications mc;
-            XSection_Selector xs;
-            xs.Channel = Channel;
-            xs.Coupling = Coupling;
-            xs.Integrand = Integrand;
-            mc.NStart = 5000;
-            mc.NIncrease = 750;
-            mc.MaxEval = 100000;
-            mc.RelErr = 1E-10;
-            mc.Params = &xs;
-            BIntegrator->Integrate(&mc,Method);
+            mc.NStart = MC.NStart;
+            mc.NIncrease = MC.NIncrease;
+            mc.MaxEval = MC.MaxEval;
+            mc.RelErr = MC.RelErr;
+
+            Montecarlo_Integrator * MCI;
+
+            if (XS.Integrand=="Born"||XS.Integrand=="Virtual"){
+                MCI = BIntegrator;
+                XS.Catalog = "Virtuals";
+            }
+            else if (XS.Integrand=="Endpoint"){
+                MCI = BIntegrator;
+                XS.Catalog = "Reals";
+            }
+            else if (XS.Integrand=="PlusDistribution"){
+                MCI = PIntegrator;
+                XS.Catalog = "Reals";
+            }
+            else if (XS.Integrand=="Subtracted"){
+                MCI = SIntegrator;
+                XS.Catalog = "Reals";
+            }
+            else{
+                std::cout<<"Error: Unavailable integrand "<<XS.Integrand<<" for "<<XS.Channel<<" channel"<<std::endl;
+                abort();
+            }
+
+            mc.Params = &XS;
+            MCI->Integrate(&mc,Method);
+               
         }
 };
 
