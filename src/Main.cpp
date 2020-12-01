04fa06a6991aa1f586f15427472e7e02e3957ab4
Diogenes Figueroa <diogenes1991@gmail.com>
2020-12-01 10:37:30 -0500

    Removal of deprecated methods XSectionStart and XSectionDelete Addition of new class Environment to manage input file and XSection_Integrator classes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,43 +1,75 @@
 #include "XSection_Integrator.h"
 
-void XSectionStart(std::string pdfname){
+class Environment{
 
-    X1 = new XSection(pdfname);
-}
+    std::unordered_map<std::string,std::string> InputFile;
 
-void XSectionEnd(){
-
-    delete X1;
-}
-
-void LoadInput(const std::string & filename, std::unordered_map<std::string,std::string>& settings){
-  std::ifstream data;
-  std::string linebuf;
-  data.open(filename.data());
-    if (!data.fail()) {
-        while(!std::getline(data,linebuf).eof()) {
-            if(linebuf=="") continue;
-            std::istringstream ss(linebuf);
-            std::string name;
-            char equal;
-            std::string val;
-            ss >> name;
-            ss >> equal;
-            if (!ss.fail() && equal == '=') {
-                ss >> val;
-                settings.insert({name,val});
+    static void LoadInput(const std::string & filename, std::unordered_map<std::string,std::string>& settings){
+        std::ifstream data;
+        std::string linebuf;
+        data.open(filename.data());
+            if (!data.fail()) {
+                while(!std::getline(data,linebuf).eof()) {
+                    if(linebuf=="") continue;
+                    std::istringstream ss(linebuf);
+                    std::string name;
+                    char equal;
+                    std::string val;
+                    ss >> name;
+                    ss >> equal;
+                    if (!ss.fail() && equal == '=') {
+                        ss >> val;
+                        settings.insert({name,val});
+                    }
+                    else{
+                        std::cout<<"Warning: Malformed line at input"<<linebuf<<std::endl;
+                    }
+                }
+            data.close();
             }
-            else{
-                std::cout<<"Warning: Malformed line at input"<<linebuf<<std::endl;
+            else {
+                std::cout << "Error: No input file found" << std::endl;
+                abort();
             }
+    }
+
+    XSection_Integrator * XSec_Int;
+
+    Montecarlo_Integrator::Specifications MC_SP;
+    XSection_Integrator::XSection_Selector XS;
+
+    public:
+
+        Environment(std::string InputFileName){
+            LoadInput(InputFileName,InputFile);
+            std::string PDFSet     = InputFile.at("LHAPDFSet");
+            std::string Integrator = InputFile.at("Integrator");
+            
+            MC_SP.Method    = InputFile.at("Method");
+            MC_SP.MaxEval   = stoi(InputFile.at("NEvaluations"));
+            MC_SP.NStart    = stoi(InputFile.at("NStart"));
+            MC_SP.NIncrease = stoi(InputFile.at("NIncrease"));
+            
+            XS.Integrand = InputFile.at("Integrand");
+            XS.Channel   = InputFile.at("Channel");
+            XS.Coupling  = InputFile.at("Coupling");
+            
+            double sqrts = stod(InputFile.at("sqrts"));
+            double muRen = stod(InputFile.at("muRen"));
+            double muFac = stod(InputFile.at("muFac"));
+            
+            XSec_Int = new XSection_Integrator(PDFSet,Integrator);
+            XSec_Int->XSec->SetScales(sqrts,muRen,muFac);
         }
-      data.close();
-    }
-    else {
-        std::cout << "Error: No input file found" << std::endl;
-        abort();
-    }
-}
+
+        ~Environment(){
+            delete XSec_Int;
+        }
+
+        void Run(){
+            XSec_Int->ComputeXSection(XS,MC_SP);
+        }
+};
 
 int main(int argc, char* argv[]){
 
@@ -50,29 +82,8 @@ int main(int argc, char* argv[]){
         abort();
     }
 
-    std::unordered_map<std::string,std::string> Settings;
-    LoadInput(argv[1],Settings);
-            
-    std::string PDFSet = Settings.at("LHAPDFSet");
-
-    std::string Integrator = Settings.at("Integrator");
-    std::string Method = Settings.at("Method");
-
-    Montecarlo_Integrator::Specifications MC_SP;
-    MC_SP.MaxEval = stoi(Settings.at("NEvaluations"));
-    MC_SP.NStart = stoi(Settings.at("NStart"));
-    MC_SP.NIncrease = stoi(Settings.at("NIncrease"));
-
-    XSection_Integrator::XSection_Selector XS;
-    XS.Integrand = Settings.at("Integrand");
-    XS.Channel = Settings.at("Channel");
-    XS.Coupling = Settings.at("Coupling");
-
-    XSectionStart(PDFSet);
-    X1->SetScales(13000,91.1876,91.1876);
-    XSection_Integrator C1(Integrator);
-    C1.ComputeXSection(XS,MC_SP,Method);
-    XSectionEnd();
+    Environment E1(argv[1]);
+    E1.Run();
 
     return 0;
 }
