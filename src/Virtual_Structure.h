34ec4cb2ac56245b1651bb02a56fcc2a34a2a727
Diogenes Figueroa <diogenes1991@gmail.com>
2020-12-01 12:29:30 -0500

    Common code extraction into the abstract class Virtual Structure

--- src/Virtual_Structure.h
+++ src/Virtual_Structure.h
@@ -11,16 +11,57 @@ class VirtualStructure {
     public:
 
         Process* Proc;
+        FourVector P;
+        int nPar;
+        double* BornMasses;
+        int* BornPID;
+        FourVector * BornMomenta;
 
-        virtual ~VirtualStructure() {};
+        virtual ~VirtualStructure(){
+            delete [] BornMomenta;
+            delete [] BornMasses;
+            delete [] BornPID;
+        }
+        
+        void SetECM(double ECM){
+            P.p0=ECM;
+            SetInMom();
+        }
 
-        virtual void SetECM(double sqrts) = 0;
-        virtual void SetInMom() = 0;
-        virtual void SetFiMom(double* rand, double* J) = 0;
+        void SetFiMom(double* rFi, double* J){
+            FourVector PFi[nPar-2];
+            double mFi[nPar-2];
+            double dummy = 1.0;
+            for (int i=0;i<nPar-2;i++) mFi[i] = BornMasses[i+2];
+            Recursive_PSP(P,nPar-2,PFi,mFi,rFi,dummy);
+            for (int i=0;i<nPar-2;i++) BornMomenta[i+2] = PFi[i];
+            *J = dummy;    
+        }
 
-        virtual void GetMomenta(FourVector* p) = 0;
-        virtual void GetMasses(double* m) = 0;
-        virtual void GetPID(int* pid) = 0;
+        void SetInMom(){
+            FourVector PIn[2];
+            double mIn[2]={BornMasses[0],BornMasses[1]};
+            double rIn[2]={0.0,0.0};
+            double dummy = 1.0;
+            Recursive_PSP(P,2,PIn,mIn,rIn,dummy);
+            BornMomenta[0] = PIn[0];
+            BornMomenta[1] = PIn[1];
+        }
+
+        void GetMomenta(FourVector* p){
+
+            for(int i=0;i<nPar;i++)p[i]=BornMomenta[i];
+        }
+
+        void GetMasses(double* m){
+
+            for(int i=0;i<nPar;i++)m[i]=BornMasses[i];
+        }
+
+        void GetPID(int* pid){
+
+            for(int i=0;i<nPar;i++)pid[i]=BornPID[i];
+        }
 
         virtual void Born(std::string cp, double* rand, double* rval) = 0;
         virtual void Virtual(std::string cp, double* rand, double mu, double* rval) = 0;
--- tpl/Virtual_Functions_tpl.h
+++ tpl/Virtual_Functions_tpl.h
@@ -9,27 +9,10 @@
 
 class ####SubProcName#### : public VirtualStructure{
 
-    FourVector P;
-    double* BornMasses;
-    int* BornPID;
-    FourVector BornMomenta[NextV];
-
     public:
         
         ####SubProcName####(Process& process);
-        ~####SubProcName####(){
-            delete [] BornMasses;
-            delete [] BornPID;
-        }
         
-        void SetECM(double sqrts);
-        void SetInMom();
-        void SetFiMom(double* rand, double* J);
-
-        void GetMomenta(FourVector* p);
-        void GetMasses(double* m);
-        void GetPID(int* pid);
-
         void Born(std::string cp, double* rand, double* rval);
         void Virtual(std::string cp, double* rand, double mu, double* rval);
 
