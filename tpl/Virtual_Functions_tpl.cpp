3239ac2ce1eb5848f2872201f92df1e6beb71dbd
Diogenes Figueroa <diogenes1991@gmail.com>
2020-12-01 13:48:58 -0500

    Code extraction of common structures into VirtualStructure

--- tpl/Virtual_Functions_tpl.cpp
+++ tpl/Virtual_Functions_tpl.cpp
@@ -2,51 +2,14 @@
 
 ####SubProcName####::####SubProcName####(Process& process){
 
-    
     Proc = &process; 
+    nPar = NextV; 
+    BornMomenta = new FourVector[NextV];
+    BornMasses = new double[NextV];
+    BornPID = new int[NextV];
 
 ####SubProcConst####
 
-}
-
-void ####SubProcName####::SetECM(double ECM){
-    P.p0=ECM;
-    SetInMom();
-}
-
-void ####SubProcName####::SetFiMom(double* rFi, double* J){
-    FourVector PFi[NextV-2];
-    double mFi[NextV-2];
-    double dummy = 1.0;
-    for (int i=0;i<NextV-2;i++) mFi[i] = BornMasses[i+2];
-    Recursive_PSP(P,NextV-2,PFi,mFi,rFi,dummy);
-    for (int i=0;i<NextV-2;i++) BornMomenta[i+2] = PFi[i];
-    *J = dummy;    
-}
-
-void ####SubProcName####::SetInMom(){
-    FourVector PIn[2];
-    double mIn[2]={BornMasses[0],BornMasses[1]};
-    double rIn[2]={0.0,0.0};
-    double dummy = 1.0;
-    Recursive_PSP(P,2,PIn,mIn,rIn,dummy);
-    BornMomenta[0] = PIn[0];
-    BornMomenta[1] = PIn[1];
-}
-
-void ####SubProcName####::GetMomenta(FourVector* p){
-
-    for(int i=0;i<NextV;i++)p[i]=BornMomenta[i];
-}
-
-void ####SubProcName####::GetMasses(double* m){
-
-    for(int i=0;i<NextV;i++)m[i]=BornMasses[i];
-}
-
-void ####SubProcName####::GetPID(int* pid){
-
-    for(int i=0;i<NextV;i++)pid[i]=BornPID[i];
 }
 
 void ####SubProcName####::Born(std::string cp, double* rand, double* rval){
