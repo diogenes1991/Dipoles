461434238fa621ab6415e89834daafd1d7056c22
Diogenes Figueroa <diogenes1991@gmail.com>
2020-12-16 15:36:02 -0500

    Addition of clock utility

--- src/Utilities.h
+++ src/Utilities.h
@@ -1,6 +1,8 @@
 #ifndef __UTILITIES_H__
 #define __UTILITIES_H__
 
+#include <time.h>
+
 template <class T>
 class ColorAndSpinMatrixT{
 
@@ -36,5 +38,44 @@ class ColorAndSpinMatrixT{
 };
 
 typedef ColorAndSpinMatrixT<double> ColorAndSpinMatrix;
+
+class Clock{
+
+    size_t start;
+
+    public:
+
+        Clock(){
+            start = time(NULL);
+        }
+        ~Clock(){}
+        
+        void Reset(){
+            start = time(NULL);
+        }
+
+        void ShowTime(){
+
+            size_t t = time(NULL)-start;
+
+            size_t sec = 1;
+            size_t min = 60*sec;
+            size_t hour = 60*min;
+            size_t day = 24*hour;
+
+            size_t t_sequence[4] = {day,hour,min,sec};
+            std::string n_sequence[4] = {"day","hour","min","sec"};
+            
+            for(int i=0;i<4;i++){
+                int t_i = t/t_sequence[i];
+                if(t_i)std::cout<<t_i<<" "<<n_sequence[i]<<(t_i == 1 ? " " : "s ");
+                t -= t_i*t_sequence[i];
+            }
+
+            std::cout<<std::endl;
+        }
+
+            
+};
 
 #endif
