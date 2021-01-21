9bbb919ebf39e5e89118ea0dc49c77bd0b030f83
Diogenes Figueroa <diogenes1991@gmail.com>
2021-01-21 14:29:51 -0500

    Addition of Phase_Space_Generator class to encapsulate different parametrizations

---
+++ src/Generator.h
@@ -0,0 +1,21 @@
+#ifndef __GENERATOR_H__
+#define __GENERATOR_H__
+
+class Generator {
+
+    int NPar;
+    double ECM;
+    std::string Available[1] = {"Recursive"};
+
+    public:
+
+        Generator(){
+
+        }
+        
+        void GetPSP(double* rand, FourVector* )
+};
+
+#endif
+
+
