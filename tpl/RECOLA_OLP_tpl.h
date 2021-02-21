08434c42d2871c28bb40a0e322a7f426bd45136f
Diogenes Figueroa <diogenes1991@gmail.com>
2021-02-21 10:24:31 -0500

    Parameters are now initialized in the corect order

--- tpl/RECOLA_OLP_tpl.h
+++ tpl/RECOLA_OLP_tpl.h
@@ -26,6 +26,47 @@ class RECOLA_OLP : public OLP{
         void UpdateParameters(){
 
             //
+            //  Masses: RECOLA
+            //  
+            
+            if(model->UseCMScheme) Recola::set_complex_mass_scheme_rcl();
+
+            if(model->d.Mass){
+                Recola::set_pole_mass_down_rcl(model->d.Mass);
+                Recola::unset_light_down_rcl();
+            }
+            else Recola::set_light_down_rcl();
+            
+            if(model->u.Mass){
+                Recola::set_pole_mass_up_rcl(model->u.Mass);
+                Recola::unset_light_up_rcl();
+            }
+            else Recola::set_light_up_rcl();
+
+            if(model->s.Mass){
+                Recola::set_pole_mass_strange_rcl(model->s.Mass);
+                Recola::unset_light_strange_rcl();
+            }
+            else Recola::set_light_strange_rcl();
+            
+            if(model->c.Mass){
+                Recola::set_pole_mass_charm_rcl(model->c.Mass,model->c.Width);
+                Recola::unset_light_charm_rcl();
+            }
+            else Recola::set_light_charm_rcl();
+            
+            if(model->b.Mass){
+                Recola::set_pole_mass_bottom_rcl(model->b.Mass,model->b.Width);
+                Recola::unset_light_bottom_rcl();
+            }
+            else Recola::set_light_bottom_rcl();
+            
+            Recola::set_pole_mass_top_rcl(model->t.Mass,model->t.Width);
+            Recola::set_pole_mass_w_rcl(model->Wp.Mass,model->Wp.Width);
+            Recola::set_pole_mass_z_rcl(model->Z.Mass,model->Z.Width);
+            Recola::set_pole_mass_h_rcl(model->h.Mass,model->h.Width);
+            
+            //
             //  QCD Scheme
             //
 
@@ -34,8 +75,6 @@ class RECOLA_OLP : public OLP{
             //
             //  EWK Scheme
             //
-
-            if(model->UseCMScheme) Recola::set_complex_mass_scheme_rcl();
             
             if(model->UseGMuScheme) Recola::use_gfermi_scheme_and_set_gfermi_rcl(model->GFermi);
             else{
@@ -43,52 +82,11 @@ class RECOLA_OLP : public OLP{
                 Recola::set_delta_uv_rcl(1.0);
             }
 
-
-            //
-            //  Masses: RECOLA
-            //  
-            //
-
-            if(model->d.Mass){
-                Recola::unset_light_down_rcl();
-                Recola::set_pole_mass_down_rcl(model->d.Mass);
-            }
-            
-            if(model->u.Mass){
-                Recola::unset_light_up_rcl();
-                Recola::set_pole_mass_up_rcl(model->u.Mass);
-            }
-
-            if(model->s.Mass){
-                Recola::unset_light_strange_rcl();
-                Recola::set_pole_mass_strange_rcl(model->s.Mass);
-            }
-            
-            if(model->c.Mass){
-                Recola::unset_light_charm_rcl();
-                Recola::set_pole_mass_charm_rcl(model->c.Mass,model->c.Width);
-            }
-
-            if(model->b.Mass){
-                Recola::unset_light_bottom_rcl();
-                Recola::set_pole_mass_bottom_rcl(model->b.Mass,model->b.Width);
-            }
-            
-            if(model->t.Mass){
-                Recola::set_pole_mass_top_rcl(model->t.Mass,model->t.Width);
-            }
-            
-            if(model->Wp.Mass){
-                Recola::set_pole_mass_w_rcl(model->Wp.Mass,model->Wp.Width);
-            }
-
-            if(model->Z.Mass){
-                Recola::set_pole_mass_z_rcl(model->Z.Mass,model->Z.Width);
-            }
-            
-            if(model->h.Mass){
-                Recola::set_pole_mass_h_rcl(model->h.Mass,model->h.Width);
-            }
+            Recola::set_dynamic_settings_rcl(1);
+            Recola::set_mu_uv_rcl(1000);
+            Recola::set_mu_ir_rcl(1000);
+            Recola::set_delta_ir_rcl(0,0);
+               
         }
         
         void Evaluate(Arguments * arg){
